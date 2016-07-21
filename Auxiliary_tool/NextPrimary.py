ae7bd1e705a647442a9ab284e6efc9519f3726b7
vashonhu <hu1251562003@gmail.com>
2016-07-21 09:07:30 +0800

    'hashTable'

---
+++ Auxiliary_tool/NextPrimary.py
@@ -0,0 +1,13 @@
+def nextPrimary(n):
+    if  n % 2 == 0:
+        n += 1
+    while True:
+        noPrimary = 0
+        for i in range(3, n / 2 + 1, 2):
+            if n % i == 0:
+                noPrimary = 1
+                break
+        if not noPrimary:
+            return n
+        else:
+            n += 2
